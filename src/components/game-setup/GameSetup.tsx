80cb27a743ce4e4a2c80b52e3de32e2456792530
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-07 15:18:00 +0200

    remove unused files

--- src/components/game-setup/GameSetup.tsx
+++ src/components/game-setup/GameSetup.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { observer } from "mobx-react";
 import { useMainStore } from "../../hooks/useMainStore";
 import { GameField } from "../game/GameField";
-import { ContinueOverlay } from "../overlays/ContinueOverlay";
-import { GameStage } from "../../stores/gameStore";
 import { StartOverlay } from "../overlays/StartOverlay";
 
 export const GameSetup = observer(() => {
@@ -13,7 +11,6 @@ export const GameSetup = observer(() => {
     <>
       <GameField />
       {game.gameRound === 0 && <StartOverlay />}
-      {game.gameStage === GameStage.Continue && <ContinueOverlay />}
     </>
   );
 });
