502633ae109dd8b81758a357ad599599294da9cc
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-07 13:45:59 +0200

    disable deal-button when game is playing

--- src/components/betting-chips/BettingControls.tsx
+++ src/components/betting-chips/BettingControls.tsx
@@ -16,6 +16,7 @@ export const BettingControls = observer(() => {
 
   const handleDeal = () => {
     game.setGameStage(GameStage.InitialCards);
+    game.setGameStage(GameStage.InitialCards);
   };
 
   return (
