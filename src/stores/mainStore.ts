f9e03ff06a5c88758dac29767771cd3fae4aac54
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-07 15:14:12 +0200

    replace continue/end overlay and end game when money is up

--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -77,10 +77,6 @@ export class MainStore {
         runInAction(() => {
           switch (this.game.gameStage) {
             case GameStage.InitialBet:
-              // this.baccarat.givePlayerACard();
-              // this.baccarat.givePlayerACard();
-              // this.baccarat.giveBankerACard();
-              // this.baccarat.giveBankerACard();
               break;
             case GameStage.InitialCards:
               this.baccarat.givePlayerACard();
@@ -96,10 +92,6 @@ export class MainStore {
                 // eslint-disable-next-line no-param-reassign
                 card.flipped = true;
               });
-              // this.baccarat.givePlayerACard();
-              // this.baccarat.givePlayerACard();
-              // this.baccarat.giveBankerACard();
-              // this.baccarat.giveBankerACard();
               setTimeout(() => {
                 this.game.setGameStage(GameStage.CheckForThirdCard);
               }, 2000);
@@ -115,11 +107,15 @@ export class MainStore {
                 setTimeout(() => {
                   this.setWinner();
                 }, 2000);
-                // this.setWinner();
                 setTimeout(() => {
-                  this.game.setGameStage(GameStage.Continue);
+                  if (this.player.playerMoney <= 0) {
+                    this.endGameReset();
+                    this.game.setGameStage(GameStage.InitialBet);
+                  } else {
+                    this.betweenRoundsReset();
+                    this.game.setGameStage(GameStage.InitialBet);
+                  }
                 }, 4000);
-                // this.setGameStage(GameStage.Continue);
               }
               // otherwise
               else {
@@ -166,9 +162,14 @@ export class MainStore {
                 }, 2000);
                 // this.setWinner();
                 setTimeout(() => {
-                  this.game.setGameStage(GameStage.Continue);
+                  if (this.player.playerMoney <= 0) {
+                    this.endGameReset();
+                    this.game.setGameStage(GameStage.InitialBet);
+                  } else {
+                    this.betweenRoundsReset();
+                    this.game.setGameStage(GameStage.InitialBet);
+                  }
                 }, 4000);
-                // this.setGameStage(GameStage.Continue);
               }
               break;
             default:
