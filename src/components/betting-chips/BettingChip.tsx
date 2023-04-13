b5821a8125503d6e8c210d735074acfaa62cd1a2
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-13 18:24:58 +0200

    remove observable from BettingChip

--- src/components/betting-chips/BettingChip.tsx
+++ src/components/betting-chips/BettingChip.tsx
@@ -1,53 +1,24 @@
 import React from "react";
-import { observer } from "mobx-react";
-import sound from "./betting-chip-sound.mp3";
 
 import styles from "./BettingChip.module.scss";
-import { useMainStore } from "../../hooks/useMainStore";
-import { useAudio } from "../../hooks/useAudio";
 
 export type BettingChipsValues = 1 | 5 | 25 | 50 | 100;
 export type BetOnOptions = "player" | "tie" | "bank";
 
 export type BettingChipProps = {
   value: BettingChipsValues;
-  betOn: BetOnOptions;
+  isHidden: boolean;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-export const BettingChip = observer(({ value, betOn }: BettingChipProps) => {
-  const { player, betOnPlayer, betOnTie, betOnBanker } = useMainStore();
-  const audio = useAudio(sound);
-
-  const addToBet = () => {
-    if (value > player.playerMoney) {
-      return;
-    }
-
-    switch (betOn) {
-      case "player":
-        betOnPlayer(value);
-        break;
-      case "tie":
-        betOnTie(value);
-        break;
-      case "bank":
-        betOnBanker(value);
-        break;
-      default:
-        return;
-    }
-    audio.play();
-  };
-
-  return (
-    <button
-      className={styles.chip}
-      type="button"
-      value={value}
-      onClick={addToBet}
-      hidden={value > player.playerMoney}
-    >
-      {value}
-    </button>
-  );
-});
+export const BettingChip = ({ value, isHidden, onClick }: BettingChipProps) => (
+  <button
+    className={styles.chip}
+    type="button"
+    value={value}
+    onClick={onClick}
+    hidden={isHidden}
+  >
+    {value}
+  </button>
+);
--- src/components/betting-chips/BettingChips.tsx
+++ src/components/betting-chips/BettingChips.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { observer } from "mobx-react";
 import { BetOnOptions, BettingChip, BettingChipsValues } from "./BettingChip";
+import sound from "./betting-chip-sound.mp3";
+import { useAudio } from "../../hooks/useAudio";
 
 import styles from "./BettingChips.module.scss";
+import { useMainStore } from "../../hooks/useMainStore";
 
 const bettingChipValues: BettingChipsValues[] = [1, 5, 25, 50, 100];
 
@@ -10,10 +13,43 @@ export type BettingChipsProps = {
   bettingChoice: BetOnOptions;
 };
 
-export const BettingChips = observer(({ bettingChoice }: BettingChipsProps) => (
-  <div className={styles.chips}>
-    {bettingChipValues.map((value) => (
-      <BettingChip key={value} value={value} betOn={bettingChoice} />
-    ))}
-  </div>
-));
+export const BettingChips = observer(({ bettingChoice }: BettingChipsProps) => {
+  const { player, betOnPlayer, betOnTie, betOnBanker } = useMainStore();
+  const audio = useAudio(sound);
+
+  const addToBet = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { value } = event.target as HTMLButtonElement;
+
+    if (+value > player.playerMoney) {
+      return;
+    }
+
+    switch (bettingChoice) {
+      case "player":
+        betOnPlayer(+value);
+        break;
+      case "tie":
+        betOnTie(+value);
+        break;
+      case "bank":
+        betOnBanker(+value);
+        break;
+      default:
+        return;
+    }
+    audio.play();
+  };
+
+  return (
+    <div className={styles.chips}>
+      {bettingChipValues.map((value) => (
+        <BettingChip
+          key={value}
+          value={value}
+          isHidden={player.playerMoney < +value}
+          onClick={addToBet}
+        />
+      ))}
+    </div>
+  );
+});
