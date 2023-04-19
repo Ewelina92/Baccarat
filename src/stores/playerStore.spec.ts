b4b25eefddefcd4c94f62caeeb8e1c4f68c98af6
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-19 11:03:08 +0200

    remove duplicate code for player store tests

--- src/stores/playerStore.spec.ts
+++ src/stores/playerStore.spec.ts
@@ -1,17 +1,16 @@
 import { PlayerStore } from "./playerStore";
 
+const player = new PlayerStore();
+
 describe("create a playerStore", () => {
   it("should return playerMoney 0", () => {
-    const player = new PlayerStore();
-
     expect(player.playerMoney).toBe(0);
   });
 });
 
 describe("money changes", () => {
-  const player = new PlayerStore();
-
   it("should set amount of money to 1", () => {
+    expect(player.playerMoney).toBe(0);
     player.setPlayerMoney(1);
     expect(player.playerMoney).toBe(1);
   });
