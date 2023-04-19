74d6b21367878a579a0c94c76d3b671348daa158
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-19 11:01:01 +0200

    adjust tests for player store

--- src/stores/playerStore.spec.ts
+++ src/stores/playerStore.spec.ts
@@ -1,33 +1,33 @@
 import { PlayerStore } from "./playerStore";
 
-describe("create a playerStore with default settings", () => {
+describe("create a playerStore", () => {
   it("should return playerMoney 0", () => {
     const player = new PlayerStore();
 
-    expect(player.playerMoney).toEqual(0);
+    expect(player.playerMoney).toBe(0);
   });
 });
 
-describe("playerStore methods should work", () => {
+describe("money changes", () => {
   const player = new PlayerStore();
 
   it("should set amount of money to 1", () => {
     player.setPlayerMoney(1);
-    expect(player.playerMoney).toEqual(1);
+    expect(player.playerMoney).toBe(1);
   });
 
   it("should reset amount of money back to 0", () => {
     player.resetPlayer();
-    expect(player.playerMoney).toEqual(0);
+    expect(player.playerMoney).toBe(0);
   });
 
   it("should increment amount of money by 2", () => {
     player.addPlayerMoney(2);
-    expect(player.playerMoney).toEqual(2);
+    expect(player.playerMoney).toBe(2);
   });
 
   it("should decrement amount of money by 2", () => {
     player.removePlayerMoney(2);
-    expect(player.playerMoney).toEqual(0);
+    expect(player.playerMoney).toBe(0);
   });
 });
