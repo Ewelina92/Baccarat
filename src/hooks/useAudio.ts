e8bae985f4b57296ebfceaf1407f82e3eb6a7b27
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-22 12:11:39 +0200

    add documentation to custom hooks

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,5 +1,10 @@
 import React from "react";
 
+/**
+A custom hook that returns an HTMLAudioElement with the specified source URL.
+@param {string} src - The URL of the audio file.
+@returns {HTMLAudioElement} The HTMLAudioElement object representing the audio file.
+*/
 export const useAudio = (src: string) => {
   const audio = React.useRef(new Audio(src));
 
--- src/hooks/useMainStore.ts
+++ src/hooks/useMainStore.ts
@@ -1,6 +1,12 @@
 import React from "react";
 import { MainStore, MainStoreContext } from "../stores/mainStore";
 
+/**
+A custom hook that provides access to the MainStore context.
+@function useMainStore
+@returns {MainStore} The MainStore context object
+@throws {Error} Throws an error if the MainStore context is not available.
+*/
 export const useMainStore = (): MainStore => {
   const context = React.useContext(MainStoreContext);
   if (!context) {
