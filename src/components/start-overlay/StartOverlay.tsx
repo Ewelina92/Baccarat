07725cad548f248cd1a0cee354891e0d25e099c3
Ewelina92 <64688069+Ewelina92@users.noreply.github.com>
2023-04-24 17:57:10 +0200

    fix export

--- src/components/start-overlay/StartOverlay.tsx
+++ src/components/start-overlay/StartOverlay.tsx
@@ -4,7 +4,7 @@ import { VolumeButton } from "../volume-button/VolumeButton";
 import { Instructions } from "./instructions/Instructions";
 import { StartForm } from "./start-form/StartForm";
 import styles from "./StartOverlay.module.scss";
-import Chat from "./chat/Chat";
+import { Chat } from "./chat/Chat";
 
 export const StartOverlay = () => (
   <Portal>
